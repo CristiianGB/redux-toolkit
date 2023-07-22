7882deb770435f3758d462010f5121ecbccb8045
Cristian Guardeño <cristian.guardenyo@gmail.com>
2023-07-22 16:12:13 +0200

    pruebas con Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MiRouter from "./MiRouter"
+import store from './store/store';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* al provider le doy el valor del store para relacionarlo y que toda la aplicaciona acceda a sus valores */}
+    <Provider store={store}> 
+    {/* almaceno todo el proyecto en miRouter */}
+      <MiRouter /> 
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
